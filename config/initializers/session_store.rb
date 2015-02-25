9a12ef64b5c3d2ff8b78601e1d90fff82466ba67
klimmen <klim030187@mail.ru>
2015-02-25 11:58:55 +0200

    start project

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.session_store :cookie_store, key: '_universal_web_interface_for_different_models_of_switches_session'
