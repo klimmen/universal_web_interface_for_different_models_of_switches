60a3dcb5b6a9a8c9440a2c11f9fc2a7959bdad6f
klimmen <klim030187@mail.ru>
2015-04-01 15:29:18 +0300

    add gems guard, guard-rspec, spring-commands-rspec

--- Gemfile
+++ Gemfile
@@ -50,6 +50,9 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 
+  gem 'guard'
+  gem 'guard-rspec'
+  gem 'spring-commands-rspec'
   gem "rspec"
   gem "rspec-rails"
   gem 'factory_girl_rails'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,22 +23,12 @@ RSpec.configure do |config|
   config.after(:each) do
    DatabaseCleaner.clean
   end
-end
-
-
-RSpec.configure do |config|
   config.include Devise::TestHelpers, :type => :controller
-#  config.extend ControllerMacros, :type => :controller
-end
-
-
-RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
-  #config.expect_with :rspec do |expectations|
-  #  expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  #end
-
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
 end
+
+
+
